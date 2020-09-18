70a4ee268630cf51e1c45b19587c6f164fb89548
sirdar9009 <71365593+sirdar9009@users.noreply.github.com>
2020-09-18 10:47:03 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var nama = "Muhammad Iqbal";
+var umur = 32;
+var wanita = false;
+let a = null;
+let b = undefined;
+var siswa=["Tri", "Maul", "Sidik"]
+
+console.log(nama)
+console.log(umur)
+console.log(wanita)
+console.log(a)
+console.log(b)
+console.log(siswa[2])
